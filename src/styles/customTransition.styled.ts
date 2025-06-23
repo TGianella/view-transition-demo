403c7e716ef14977f4278ae77e2106d0c896854a
Théo Gianella <theo.gianella@proton.me>
2025-06-23 22:05:50 +0200

    add default entry/exit transitions

--- src/styles/customTransition.styled.ts
+++ src/styles/customTransition.styled.ts
@@ -26,4 +26,14 @@ export const customTransition = css.global`
         animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
         300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
     }
+    
+    ::view-transition-old(*):only-child {
+        animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+        300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+    }
+    
+    ::view-transition-new(*):only-child {
+        animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+        300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+    }
 `
